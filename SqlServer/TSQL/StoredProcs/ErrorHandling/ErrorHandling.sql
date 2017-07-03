1e24e3888f30db79fe8597b5151681278a4fdd9b
venkat <bodempudi@users.noreply.github.com>
2017-07-03 17:29:03 +0530

    Update ErrorHandling.sql

--- SqlServer/TSQL/StoredProcs/ErrorHandling/ErrorHandling.sql
+++ SqlServer/TSQL/StoredProcs/ErrorHandling/ErrorHandling.sql
@@ -18,3 +18,35 @@ BEGIN CATCH
 END CATCH
 
 SELECT * FROM dbo.SETExactAbortTest;
+
+
+RAISERROR('This is raise error demonstration, severity 16',16,1);
+
+
+RAISERROR('This is raise error demonstration, severity 10',10,1);
+
+
+RAISERROR('This is raise error demonstration, severity 11',11,1);
+
+
+RAISERROR('This is raise error demonstration, severity 19',19,1) WITH LOG;
+
+
+RAISERROR('This is raise error demonstration, severity 20',20,1) WITH LOG;
+
+
+DECLARE @ErrorMsg VARCHAR(100)='This is raise error demonstration, severity 11';
+RAISERROR(@ErrorMsg,11,1);
+
+RAISERROR('Could not find object "%s"',13,1,'dbo.SETExactAbortTest');
+
+
+INSERT INTO dbo.SETExactAbortTest VALUES(1,'NANI',4000);
+
+
+
+SELECT * FROM sys.messages a where a.text like '%PRIMARY KEY%' ;
+SELECT * FROM sys.messages a where a.message_id=2627;
+
+Violation of %ls constraint '%.*ls'. Cannot insert duplicate key in object '%.*ls'. The duplicate key value is %ls.
+Violation of PRIMARY KEY constraint 'PK__SETExact__3213E83F287051E9'. Cannot insert duplicate key in object 'dbo.SETExactAbortTest'. The duplicate key value is (1).
