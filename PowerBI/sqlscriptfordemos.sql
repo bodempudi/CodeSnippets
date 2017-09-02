4e3613b44eb315f56ad2e06b7192faf09f20a559
Venkat Bodempudi <bodempudi@users.noreply.github.com>
2017-09-02 18:22:32 +0530

    Update sqlscriptfordemos.sql

--- PowerBI/sqlscriptfordemos.sql
+++ PowerBI/sqlscriptfordemos.sql
@@ -30,3 +30,31 @@ group by a.Color
 SELECT sum(Qunatity*PRICE) FROM dbo.MixingFiltersDemo A WHERE A.Channel='Internet'
 
 SELECT sum(Qunatity*PRICE) FROM dbo.MixingFiltersDemo A 
+
+
+--other table functions
+if OBJECT_ID('dbo.Products') IS NOT NULL
+	DROP TABLE dbo.Products;
+CREATE TABLE dbo.Products(
+id int,
+name varchar(50));
+
+CREATE TABLE dbo.Sales(
+ProductId int,
+Sales DECIMAL(10,2));
+
+INSERT INTO dbo.Products values(1,'Lenovo');
+INSERT INTO dbo.Products values(2,'HP');
+INSERT INTO dbo.Products values(3,'Dell');
+
+INSERT INTO dbo.Sales values(1,150);
+INSERT INTO dbo.Sales values(2,250);
+INSERT INTO dbo.Sales values(3,350);
+INSERT INTO dbo.Sales values(1,1050);
+INSERT INTO dbo.Sales values(2,2050);
+INSERT INTO dbo.Sales values(3,3050);
+INSERT INTO dbo.Sales values(4,450);
+
+
+SELECT * FROM dbo.Products	
+select * from dbo.Sales
