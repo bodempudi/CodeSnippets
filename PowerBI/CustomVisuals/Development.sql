17587521978a8702051dc3038a4627d7dabb62c2
Venkat <bodempudi@users.noreply.github.com>
2017-10-07 10:21:45 -0700

    Update Development.sql

--- PowerBI/CustomVisuals/Development.sql
+++ PowerBI/CustomVisuals/Development.sql
@@ -9,26 +9,12 @@ pbiviz --install-cert
 pbiviz new testVisual
 cd testVisual
 npm i -g typings
-npm i d3@3.5.5 --save
-typings install d3=github:DefinitelyTyped/DefinitelyTyped/d3/d3.d.ts#6e2f2280ef16ef277049d0ce8583af167d586c59 --global --save
+npm install powerbi-visuals-utils-dataviewutils --save
+npm i d3
+typings install d3 --global --save
 
 //add the following to tsconfig.json
 ,"typings/index.d.ts"
 
 //Add d3 reference to pbiviz.json to externalJS array
 ,"node_modules/d3/d3.min.js"
-
-npm install -g power-custom-visuals
-npm install typescript
-npm install d3 --save
-npm install less
-pbiviz --install-cert
-npm install powerbi-visuals-utils-dataviewutils --save
-
-npm @types/d3@3 --save
-
-typings install d3 --source dt --global
-typings search d3
-npm i -g typings
-npm i d3@3.5.5 --save
-typings install dt~d3 --save --global
