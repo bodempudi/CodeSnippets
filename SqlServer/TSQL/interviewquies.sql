a70fbb4926e03b585dcc08ff54665d9d875cefc4
Venkat <bodempudi@users.noreply.github.com>
2017-11-13 16:36:49 +0530

    Update interviewquies.sql

--- SqlServer/TSQL/interviewquies.sql
+++ SqlServer/TSQL/interviewquies.sql
@@ -27,3 +27,16 @@ SELECT @MAXSAL;
  select A.name,A.Sal,B.name [DNAME],DENSE_RANK() OVER(PARTITION BY B.Name ORDER BY A.SAL DESC) RANK from Allegiance.Emp A INNER JOIN Allegiance.DeptTest B ON A.deptid = B.id
  )
  SELECT A.name,A.Sal,A.[DNAME] FROM MAXSALS A WHERE A.[RANK]=1;
+
+
+
+IF CONVERT(VARCHAR(MAX),(
+    SELECT * 
+      FROM authors ORDER BY au_id FOR XML path, root))
+  =
+      CONVERT(VARCHAR(MAX),(
+    SELECT * 
+      FROM authorscopy ORDER BY au_id FOR XMLpath, root))
+SELECT 'they are  the same'
+ELSE
+SELECT 'they are different'
