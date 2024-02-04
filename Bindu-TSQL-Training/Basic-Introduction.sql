89ff69e9eab6a45fa61e4dc5a747dce18e5dcfd3
venkat-bodempudi <132123228+venkat-bodempudi@users.noreply.github.com>
2024-02-04 19:53:07 +0300

    Create Basic-Introduction.sql
    Introduction

---
+++ Bindu-TSQL-Training/Basic-Introduction.sql
@@ -0,0 +1,12 @@
+/*
+Make sure you installed SQL Server in your laptop.
+
+*/
+--Make sure SQL Server Management Studio is installed on your computer.
+--Writing comments in SQL Server
+--We can write comments in SQL Server in two ways.
+/* */ multiline comments
+--Single Line Comments
+--For every line you start with --Then SQL Server engine starts considers such lines as comments.
+
+--
