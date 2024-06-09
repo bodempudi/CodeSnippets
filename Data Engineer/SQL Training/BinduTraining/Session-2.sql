253a3233f449172a2eff607d5d8212b104166584
Venkat <bodempudi@users.noreply.github.com>
2024-06-09 20:26:02 +0300

    Update Session-2.sql

--- Data Engineer/SQL Training/BinduTraining/Session-2.sql
+++ Data Engineer/SQL Training/BinduTraining/Session-2.sql
@@ -1,1 +1,19 @@
+--GO is a batch separator
+--In a SQL Server Management Studio Query window, we can write many T-SQL Statements.
+--These queries can be DML or DDL commands
+--when we want to send the T-SQL code as a Batch we use GO in SSMS query window. GO itself is not a T-SQL command, it is command utility that signals
+to execute the commands as a single batch in SQL Server
+
+-
+alter proc proc1(@id int)
+as
+begin
+	print 'proc1'
+end
+ 
+alter proc proc2(@id int)
+as
+begin
+	print 'proc2'
+end
 
