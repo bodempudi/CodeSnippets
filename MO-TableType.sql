7f458dbe55c62d6b6512555f59a79f149d7ca2d6
Venkat <bodempudi@users.noreply.github.com>
2026-02-07 17:03:57 +0530

    Update MO-TableType.sql

--- MO-TableType.sql
+++ MO-TableType.sql
@@ -2,3 +2,16 @@
 --Simple datatypes, not even nvarchar(max) is supported.
 
 --When we are creating InMem tables, schema = SCHEMA_AND_DATA
+--ask this question, when everything is stored in memory, why memory optimized file group is required?
+CREATE TABLE dbo.Merchant
+(
+  MerchantID IDENTITY BIGINT,
+  MerchantNumber VARCHAR(128),
+  MerchantName VARCHAR(512),
+  CategoryID BIGINT,
+  CREATEDateTime DATETIME,
+  CreateUserID VARCHAR(20),
+  UpdateDatetime DATETIME,
+  UpdateUserID VARCHAR(20)
+)
+WITH (MEMORY_OPTIMIZED = ON, DURABILITY = SCHEMA_AND_DATA);
