591a64800f6fb5c79a88a215160b49b5de7e377b
Venkat <bodempudi@users.noreply.github.com>
2026-02-07 16:24:09 +0530

    Update MO-TableType.sql

--- MO-TableType.sql
+++ MO-TableType.sql
@@ -1,1 +1,4 @@
 --You can not alter memory optimized user defined table type.
+--Simple datatypes, not even nvarchar(max) is supported.
+
+--When we are creating InMem tables, schema = SCHEMA_AND_DATA
