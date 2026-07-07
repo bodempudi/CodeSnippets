99b712fdcec4c88ac2647914d8ad112fd8695ed6
Venkat <bodempudi@users.noreply.github.com>
2026-07-07 22:14:49 +0530

    Create sample.sql

---
+++ venkatexpr/sample.sql
@@ -0,0 +1,30 @@
+{
+  "UserData": [
+    {
+      "RowNo": 1,
+      "MerchantId": 1001,
+      "MerchantName": "Lulu",
+      "Result": "Passed"
+    },
+    {
+      "RowNo": 2,
+      "MerchantId": 1002,
+      "MerchantName": "Carrefour",
+      "Result": "Failed"
+    }
+  ],
+  "SystemData": [
+    {
+      "RowNo": 1,
+      "MerchantId": 1001,
+      "MerchantName": "Lulu",
+      "Result": "Passed"
+    },
+    {
+      "RowNo": 2,
+      "MerchantId": 1002,
+      "MerchantName": "Carrefour",
+      "Result": "Failed"
+    }
+  ]
+}
