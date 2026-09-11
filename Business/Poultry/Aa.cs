4fa122aee2d71819e124c3c8081237defae879e2
Venkat <bodempudi@users.noreply.github.com>
2026-09-11 10:16:27 +0300

    Create Aa.cs

---
+++ Business/Poultry/Aa.cs
@@ -0,0 +1,9 @@
+int inlineTableFunctionIndent =
+    GetInlineTableValuedFunctionQueryIndent(
+        query,
+        layoutContext);
+
+if (inlineTableFunctionIndent >= 0)
+{
+    return inlineTableFunctionIndent;
+}
